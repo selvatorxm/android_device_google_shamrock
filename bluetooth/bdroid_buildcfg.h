26fa7e5853050261d27d22c27abb0a9555be37da
Can Emeç <34311071+QuarkzzOT@users.noreply.github.com>
2017-12-17 11:07:59 +0300

    hay böyle işin anasını

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,10 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +21,14 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#include <cutils/properties.h>
-#include <string.h>
-
-#define BTM_DEF_LOCAL_NAME "General Mobile 5+"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QTI_SW TRUE
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BTM_LE_SECURE_CONN  TRUE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
