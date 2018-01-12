f1735e6e4614447fa777052c4fc6c9d3a295badd
Can Emeç <canemeckisisel@gmail.com>
2018-01-12 22:06:04 +0300

    shamrock: seccomp: add mediacodec policy

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/radio/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/radio/data/qmi_config.xml:system/etc/data/qmi_config.xml
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+	
 # Sensors
 PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
